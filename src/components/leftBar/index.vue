f0830912b3176b87b1296a2978d60e08c5391769
zhuyu <1079688386@qq.com>
2020-03-13 12:36:15 +0800

    UI整体框架已搭建完毕

---
+++ src/components/leftBar/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="leftBar">
+    <div class="buttons cursorPointer" v-for="(item, index) in dataList" :key="index">
+      <div class="head text-center" @click="change(index)">
+        <span>{{item.title}}</span>
+        <span class="mar-r-15"><img class="iconImg" :class="{rotate: item.needShow}" style="line-height:10px" src="@/assets/images/pull.png"/></span>
+      </div>
+      <ul v-if="item.needShow">
+        <li v-for="(item1, index1) in item.child" :key="index1" class="childItem text-center" :class="{childItemChecked: item1.onMouse}" @mouseover="item1.onMouse = true" @mouseout="item1.onMouse = false">
+          <span class="mar-r-15"><img class="iconImg" :src="item1.onMouse ? item1.urlIconCheck : item1.urlIcon"/></span>
+          <span class="childText">{{item1.title}}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'leftBar',
+  components: {},
+  data() {
+    return {
+      activeIndex: {}
+    };
+  },
+  props: {
+    dataList: {
+      type: Array,
+      default() {
+        return [
+          {
+            title: '房源管理',
+            needShow: false,
+            child: [
+              {
+                title: '发布卖房',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              },
+              {
+                title: '卖房管理',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              },
+              {
+                title: '发布租房',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              },
+              {
+                title: '租房管理',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              }
+            ]
+          },
+          {
+            title: '房源回访',
+            needShow: false,
+            child: [
+              {
+                title: '子选项1',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              },
+              {
+                title: '子选项2',
+                onMouse: false,
+                urlIcon: require('@/assets/images/house.png'),
+                urlIconCheck: require('@/assets/images/houseCheck.png')
+              }
+            ]
+          }
+        ];
+      }
+    }
+  },
+  methods: {
+    change(key) {
+      this.dataList[key].needShow = !this.dataList[key].needShow;
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+@import '../../assets/css/theme.less';
+.leftBar {}
+.leftBar .head {height:28px;line-height:28px;font-size:.24rem;}
+.leftBar ul>li { width:90%;margin:0 auto;height:20px;line-height:20px;font-size:.18rem;color: #666666}
+.iconImg {width:8px;display:inline-block;}
+.childItemChecked {background:@darkBlue;;border-radius:10px;}
+.leftBar .childItemChecked .childText {color:@white}
+
+.rotate{
+  transform:rotate(180deg);
+   -webkit-transform:rotate(180deg);
+   -moz-transform: rotate(180deg);
+   -o-transform: rotate(180deg);
+   -ms-transform: rotate(180deg);
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,33 +1,39 @@
 <template>
   <div class="home">
-    <el-container>
-      <el-header class="back1 header">
-        <img src="@/assets/images/title.jpg" class="center" alt="">
-        <div style="float:right">
-          <ul class="titleFloat" style="">
-            <li>
+    <div>
+      <div class="back1 header">
+        <img src="@/assets/images/title.jpg" class="iconImg cursorPointer" alt="">
+        <div style="float:right;height:100%;">
+          <ul class="titleFloat text-center mar-r-20 cursorPointer" style="">
+            <li style="padding-top:.1rem">
               <img class="avatar" src="@/assets/images/avatar.jpg" alt="">
             </li>
             <li>朱忠良</li>
             <li>经纪人</li>
-            <li>|</li>
+            <li> | </li>
             <li>返回首页</li>
             <li>退出登录</li>
           </ul>
         </div>
         <div style="clear:both"></div>
-      </el-header>
-      <el-container>
-        <el-aside class="back2" width="200px">bbss</el-aside>
-        <el-main class="back3">cc</el-main>
-      </el-container>
-    </el-container>
+      </div>
+      <div class="body">
+        <div class="aside">
+          <left-bar/>
+        </div>
+        <div class="main back3">
+          <main-body/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import leftBar from '@/components/leftBar';
+import mainBody from '@/components/mainBody';
 export default {
   name: 'home',
-  components: {},
+  components: {leftBar, mainBody},
   data() {
     return {
     };
@@ -65,12 +71,15 @@ export default {
 </script>
 <style lang="less" scoped>
 @import '../../assets/css/theme.less';
-.titleFloat {display:flex; height:100%;font-size:.2rem}
-.titleFloat li {margin-right: .1rem;}
-.avatar {width:.4rem}
+.titleFloat {display:flex; height:100%;font-size:.15rem}
+.titleFloat li {margin-right: .1rem;line-height:.8rem}
+.avatar {width:.58rem;}
+.iconImg {height:100%;;line-height:.8rem}
 .back1 {background: #26ad9c; color:@white}
-.header{background: @darkBlue;height:100px;}
-.back2 {background: #bde9e4;}
-.back3 {background: yellow;}
+.header{background: @darkBlue;height:.8rem;}
 .back4 {background: blue;}
+
+.body {display: flex;height:calc(100vh - .8rem)}
+.body .aside {width:100px;height:100%;z-index:10;box-shadow: 2px 0px 2px 0px  rgba(0,0,0,0.1)}
+.body .main {flex:1;height:100%}
 </style>
